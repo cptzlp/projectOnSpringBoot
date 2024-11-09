d5d2cc44a513629dff6189caec1666efe115c256
Maxim Fadeev <maksimfadeev4673@gmail.com>
2024-11-09 14:29:11 +0300

    стартовый коммит, проект создан

---
+++ src/main/java/ru/max/springcourse/projectonspringboot/ProjectOnSpringBootApplication.java
@@ -0,0 +1,13 @@
+package ru.max.springcourse.projectonspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectOnSpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjectOnSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/max/springcourse/projectonspringboot/ProjectOnSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.max.springcourse.projectonspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectOnSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
